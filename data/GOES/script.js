96195f1a6f3037f0a0aba3e24951aca2b5f34e8f
24Olga <jimmytao10525@gmail.com>
2025-08-12 21:59:31 +0800

    Fixed a couple problems in data

--- data/GOES/script.js
+++ data/GOES/script.js
@@ -5,8 +5,18 @@ document.querySelectorAll('.content-header').forEach(header => {
         
         if (isActive) {
             contentBody.classList.add('active');
+            contentBody.querySelector('p').classList.add('expanded');
         } else {
             contentBody.classList.remove('active');
+            contentBody.querySelector('p').classList.remove('expanded');
+        }
+    });
+});
+
+document.querySelectorAll('.content-body p').forEach(paragraph => {
+    paragraph.addEventListener('touchstart', function(e) {
+        if (this.classList.contains('expanded')) {
+            e.stopPropagation();
         }
     });
 });
