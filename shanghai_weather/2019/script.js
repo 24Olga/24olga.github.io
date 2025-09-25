606acb3cba57983ecd31cc55ed78057012f6111f
24Olga <jimmytao10525@gmail.com>
2025-09-24 22:53:34 -0400

    -

--- shanghai_weather/2019/script.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('scroll', () => {
-    const navbar = document.querySelector('.navbar');
-    navbar.style.background = window.scrollY > 50 
-        ? 'rgba(255,255,255,0.95)' 
-        : 'rgba(255,255,255,0.85)';
-});
