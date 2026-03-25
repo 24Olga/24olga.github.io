1fe237b17ec6c9a1092cf480a305a2ea32fefd6d
24Olga <jimmytao10525@gmail.com>
2026-03-24 23:07:29 -0400

    Fixed a major bug, added new images of seven cyclones, and PW20240529

--- philadelphia_weather/2024/05/script.js
+++ philadelphia_weather/2024/05/script.js
@@ -3,7 +3,7 @@ function populateDateSelect(year, month) {
     dateSelect.innerHTML = '';
     
     const daysInMonth = new Date(year, month + 1, 0).getDate();
-    for (let day = 1; day <= 28; day++) {
+    for (let day = 1; day <= 29; day++) {
         const option = new Option(
             `${String(day).padStart(2, '0')}日`,
             String(day).padStart(2, '0')
